dc7299c5c0be553303206ebe5a564af3cdd35060
Magdalena A Price <maprice@align-1.csail.mit.edu>
2022-07-18 15:07:02 -0400

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/bin/sh -ec 'cd ./interface && npm install'
+/bin/sh -ec 'cd ./interface && npm cache verify && npm install'
 # TODO: M1 machines have different installation instructions
 /bin/sh -ec 'cd ./server && python3 -m venv venv && . venv/bin/activate && pip install Flask && pip install numpy && pip install torch && pip install simpletransformers && deactivate'
