0f52d778e94feb17249f4c339fe0207b75c6249a
Jessica Turner <jturner3@umbc.edu>
2023-06-22 14:27:26 -0400

    add constant for first NLPDocTool page we'll use, update todos for our combined tool (considering page order)

--- interface/src/Constants/States.js
+++ interface/src/Constants/States.js
@@ -4,9 +4,13 @@ const states = {
     introduction: 0,
     openCoding: 1,
     assistedGrouping: 2,
-    verification: 3,
-    results: 4,
-    doctoolhome: 5, // I don't htink the number really matters, since we call it with whatever next page.
+    // docJustification: have the annotator describe their thought process for the labels they provided
+    docJustification: 3, // let's just have the numbers go in order for ease of understanding
+    // todo: consider if we want to skip docJustification (old step4) and go right into step5
+    // todo: generation (what the annotator thinks the model will output)
+    // todo: comparison (what the annotator thought vs the actual model output)
+    verification: 4, // openCoding: the annotator checks how correct the model was
+    results: 5,
 }
 
 export default states;
--- interface/src/Pages/DocJustification/todo.md
+++ interface/src/Pages/DocJustification/todo.md
@@ -1,6 +1,9 @@
 My todos from the js file are pasted here.
 When complete, remember to cross off in both places!
 
+- consider if we want to skip this step (4) 
+and just go right to the annotator generating hypotheses
+
 - link this page to AssistedGrouping
 
 - add the next 2 steps of NLPDocTool to this branch and connect the pages.
