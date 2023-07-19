3ffd89e337c726ac11231698562015777d2fe059
Jessica Turner <jturner3@umbc.edu>
2023-07-19 11:16:10 -0400

    Tested http-proxy-middleware w/ correct proxy name
    It still didn't work.
    
    Our get request seemed to not be proxied at all...
    Testing get link with link="/user_model/train"...
    App.js:104     GET http://localhost:3000/user_model/train?

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # /bin/sh -ec 'export NODE_OPTIONS=--openssl-legacy-provider'
 # doing that didn't work^ so I'm going to try and add a file to run that
 # similarly, installing material-ui always says unable to resolve dependencies, so I just force it.
-/bin/sh -ec 'cd ./interface && npm cache verify && npm install --save --legacy-peer-deps  && npm install  --force node-fetch@2'
+/bin/sh -ec 'cd ./interface && npm cache verify && npm install --save --legacy-peer-deps  && npm install  --force node-fetch@2 && npm install--force http-proxy-middleware --save'
 # TODO: M1 machines have different installation instructions
 # added installing transformers library and `accelerate` dependency.
 /bin/sh -ec 'cd ./server && python3 -m venv venv && . venv/bin/activate && pip install Flask && pip install numpy && pip install torch && pip install simpletransformers && pip install transformers && pip install accelerate && pip install tqdm && deactivate'
