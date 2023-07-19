fd30be2b14208583b24b3c7ddac219d1f64de38a
Jessica Turner <jturner3@umbc.edu>
2023-07-19 15:35:04 -0400

    Attempt to implement /model/predict with pretrain
    Note, we'll probably need to do something more like opencoding with training.
    But, this is just so I can see how we will maybe go back and forth between
    the user's model with get requests.
    
    I'm currently getting this error:
    - invalid literal for int() with base 10: 'last_hidden_state'
    Which I would guess is because I'm not sure how to decode the model output
    (which by itself seems to be a tensor a matrix of floats).
    
    Todo:
    - debug /predict decoding (i.e. remove skip_special_tokens, add others)
    - add a call to predict in our frontend that we can display to the user
    (i.e. so that we can compare the user's hypothesis with the model output).
    - consider how we will train a model from scratch on the opencoding results

--- test_user_model/test_app.py
+++ test_user_model/test_app.py
@@ -1,22 +1,52 @@
-from flask import Flask
+from flask import Flask, request
+from transformers import GPT2Tokenizer, GPT2Model
 
 app = Flask(__name__)
 TEST_PORT_NUMBER = 5555
 
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+model = GPT2Model.from_pretrained("gpt2")
+
 @app.route("/user_model")
 def home():
-    response = {"ok": True, "body": "<p>Welcome to the test home page! Nothing is set up yet.</p>"}
+    response = {"body": "<p>Welcome to the test home page! Nothing is set up yet.</p>"}
+    add_options(response)
     return response
 
 @app.route("/user_model/train")
 def train():
-    response = {"ok": True, "body": "<p>Welcome to the train page! Nothing is set up yet.</p>"}
+    response = {"body": "<p>Welcome to the train page! Nothing is set up yet.</p>"}
+    add_options(response)
     return response
 
-@app.route("/user_model/predict")
+@app.route("/user_model/predict", methods=['GET'])
 def predict():
-    response = {"ok": True, "body": "<p>Welcome to the predict page! Nothing is set up yet.</p>"}
+    response = {"body": "<p>Welcome to the predict page! Nothing is set up yet.</p>"}
+    
+    try:
+        input_text = request.args.get("text")
+        encoded_input = tokenizer(input_text, return_tensors="pt")
+        encoded_output = model(**encoded_input)
+        string_output = tokenizer.decode(encoded_output, skip_special_tokens=True)
+        print(string_output)
+        response["output"] = string_output
+        add_options(response)
+    except Exception as e:
+        response["ok"] = False
+        print(e)
+        response["statusText"] = str(e)
+    
     return response
+
+# make the response ok consistent
+def add_options(response):
+    '''
+    Sets default options for all return responses that are successful.
+    '''
+    response["headers"] = {"content-type": "application/json"},
+    response["ok"] = True
+    return response
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=TEST_PORT_NUMBER)
