e95deb41fed5920461afd3bf4a5b7db09b540c11
Jessica Turner <jturner3@umbc.edu>
2023-07-26 10:34:00 -0400

    Add NLPDocTool_Backend files w/ minor changes
    - changed in bin/www: env.PORT => env.NLPDOCTOOL_PORT
    - added node_modules and other logs to .gitignore
    
    Now, we are closer to being able to do get/post requests
    to NLPDocTool's backend locally rather than through petrichor.
    
    Todo:
    - update proxy from setupProxy.js and then update all the
    api calls to use the same proxy links.

---
+++ NLPDocTool_Backend/routes/index.js
@@ -0,0 +1,137 @@
+// import fetch from "node-fetch";
+
+var express = require('express');
+var router = express.Router();
+
+const crypto = require('crypto');
+const util = require('util');
+const stream = require('stream');
+const { finished } = require('stream');
+const { promisify } = require('util');
+const hash = crypto.createHash('sha1');
+hash.setEncoding('hex');
+
+const pipeline = util.promisify(stream.pipeline);
+
+var fs = require("fs");
+var { parse } = require("csv-parse");
+
+var multer  = require('multer');
+
+const finishedAsync = promisify(finished);
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+  cb(null, 'public')
+},
+filename: function (req, file, cb) {
+  cb(null, Date.now() + '-' +file.originalname )
+}
+})
+var upload = multer({ storage: storage }).single('file')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+
+router.post('/processFile', async function (req, res) {
+
+   upload(req, res, async function (err) {
+    if (err instanceof multer.MulterError) {
+        return res.status(500).json(err)
+    } else if (err) {
+        return res.status(500).json(err)
+    }
+
+    var array = [];
+    const readable = fs.createReadStream("./public/"+req.file.filename)
+    .pipe(parse({ relax_quotes: true, delimiter: ",", from_line: 1, columns: ['contextName', 'input'] }))
+    .on("data", function (row) {
+      array.push(row);
+      console.log(array);
+    })
+
+    await finishedAsync(readable);
+
+
+  console.log(array);
+
+  return res.status(200).send(array) })
+
+
+  
+  // var file = req.file;
+
+  // console.log(file);
+
+  // return res.status(200).send({file: file})
+
+  
+});
+
+
+router.post('/runPrediction', async function (req, res) {
+
+  var API_TOKEN="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";
+  console.log(req.body);
+  async function query(data) {
+    
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/deepset/roberta-base-squad2",
+        {
+          headers: { 
+            Authorization: `Bearer ${API_TOKEN}` 
+            
+          },
+          method: "POST",
+          
+          body: data,
+        }
+    );
+    const result = await response.json();
+    console.log(result)
+    return result;
+}
+  query(req.body.input).then((response) => {
+    // console.log(req.body.input);
+    var object = response
+    console.log(response);
+    var string = "sucess"
+    var string = JSON.stringify(object[req.body.outputName]);
+    // console.log(string);
+      return res.status(200).send(string)
+  });
+ 
+});
+
+router.post('/getSimilarity', async function (req, res) {
+
+  var API_TOKEN="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";
+
+  async function query(data) {
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2",
+        {
+            headers: { Authorization: `Bearer ${API_TOKEN}` },
+            method: "POST",
+            body: JSON.stringify(data),
+        }
+    );
+    const result = await response.json();
+    return result;
+}
+  query({inputs:{
+    source_sentence: req.body.one,
+    sentences: [req.body.two]
+    }}).then((response) => {
+      var score = response[0]
+      console.log(score);
+      return res.status(200).send(JSON.stringify(score))
+    });
+ 
+});
+
+module.exports = router;
--- shutdown.sh
+++ shutdown.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# all port 8000 and 3000 operations will be killed
-/bin/sh -ec 'kill -15 $(lsof -t -i:8000) && kill -15 $(lsof -t -i:3000)'
+# all port 8000 (open coding backend) and 3000 (open coding frontend) operations will be killed
+# added NLPDocTool Backend port: 3030 to be shutdown
+/bin/sh -ec 'kill -15 $(lsof -t -i:8000) && kill -15 $(lsof -t -i:3000) && kill -15 $(lsof -t -i:3030)'
