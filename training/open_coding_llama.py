432594136f986a9b1eb87a8a28feae07757b8e49
Prajna Soni <soniprajna@gmail.com>
2024-02-07 17:33:27 +0530

    fix nn.functional library call

--- training/open_coding_llama.py
+++ training/open_coding_llama.py
@@ -159,7 +159,7 @@ class OpenCodingModel(LlamaPreTrainedModel):
         hidden_states = outputs[0]
         if self.config.pretraining_tp > 1:
             lm_head_slices = self.lm_head.weight.split(self.vocab_size // self.config.pretraining_tp, dim=0)
-            logits = [F.linear(hidden_states, lm_head_slices[i]) for i in range(self.config.pretraining_tp)]
+            logits = [nn.functional.linear(hidden_states, lm_head_slices[i]) for i in range(self.config.pretraining_tp)]
             logits = torch.cat(logits, dim=-1)
         else:
             logits = self.lm_head(hidden_states)
