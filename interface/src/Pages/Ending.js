78c79d935b4ead0be4c685c134c3567ea0fcd32e
Prajna Soni <soniprajna@gmail.com>
2024-03-20 00:54:58 +0530

    add ending page

---
+++ interface/src/Pages/Ending.js
@@ -0,0 +1,15 @@
+const Ending = () => (
+    <div>
+        <b>Thank you for participating in Phase 1!</b>
+        <br></br>
+        <br></br>
+        <br></br>
+        This is the end of the Phase 1. 
+        <br></br>
+        Please complete our <a href="https://mit.co1.qualtrics.com/jfe/form/SV_2bpe9LrydyC4E62" target="_blank" rel="noopener noreferrer">survey</a>.
+        <br></br>
+        We will be in touch with details about Phase 2 soon.
+    </div>
+  );
+  
+export default Ending;
